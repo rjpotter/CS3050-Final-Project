a2568c911b758b04b01c63b9e5ea238dfa699346
Ethan Rowland <ewrowlan@uvm.edu>
2024-03-04 16:23:23 -0500

    Modified compare_units to flesh it out a bit

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from dataclasses import dataclass
 
 
 class Unit(Enum):
@@ -19,16 +20,21 @@ class Unit(Enum):
     equal_flag = -2
 
 
+
 class Game:
     def __init__(self):
         self.human_player = Human_Player()
         self.computer_player = Computer_Player()
-        self.board = [[]]
+        self.board: list[list[Unit]] = [[]]
 
 
 class Human_Player:
     def __init__(self):
-        self.troop_locations = [()]
+        self.troop_locations: list[tuple]
+
+    def initialize_locations(self):
+        # will eventually initialize troop locations here
+        pass
 
 
 class Computer_Player:
@@ -36,11 +42,21 @@ class Computer_Player:
         self.troop_locations = [()]
 
 
-def compare_troops(unit1: Unit, unit2: Unit) -> Unit:
+def compare_units(unit1: Unit, unit2: Unit) -> list[Unit]:
+    """
+    compare units takes in 2 units and returns a list of units that "survive"
+    Example: if unit1 is a scout, unit2 is a major, then the return is [unit2]
+             if both units are scouts, the return is []
+    There are a lot of edge cases when dealing with this
+    :param unit1: first unit to be compared
+    :param unit2: second unit to be compared
+    :return: list of units that "survive" encounter
+    """
     # lots of edge cases will eventually go in here
+    # VERY basic cases
     if unit1.value > unit2.value:
-        return unit1
+        return [unit1]
     elif unit2.value > unit1.value:
-        return unit2
+        return [unit2]
     else:
-        return Unit.equal_flag
+        return []
