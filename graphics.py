ce30c12a75ecac9272168c0b2d0df312e19ec95d
Cooper Zion <cooperzion2002@gmail.com>
2024-04-10 15:32:31 -0400

    Got the right amount of every piece rendered with the right texture

--- graphics.py
+++ graphics.py
@@ -11,20 +11,23 @@ SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 SCREEN_TITLE = "Stratego"
 
-TEXTURE_IMAGE_WIDTH = 1430   # the x width of the image of the pieces in pixels
-TEXTURE_IMAGE_HEIGHT = 794   # the y height of the image of the pieces in pixels
+TEXTURE_IMAGE_WIDTH = 1430  # the x width of the image of the pieces in pixels
+TEXTURE_IMAGE_HEIGHT = 794  # the y height of the image of the pieces in pixels
 PIECE_WIDTH = int(TEXTURE_IMAGE_WIDTH / 6)
 PIECE_HEIGHT = int(TEXTURE_IMAGE_HEIGHT / 2)
+
 
 # index map for pieces and their textures
 # Pieces:        | 10 | 9 | 8 | 7| 6| -1 (flag) | 5 | 4 | 3 | 2 | 1  | 11 (bomb) |
 # Texture index: | 0  | 1 | 2 | 3| 4| 5         | 6 | 7 | 8 | 9 | 10 | 11        |
 
-class Piece():
-    def __init__(self, value, color, texture: arcade.texture):
+class Piece:
+    def __init__(self, value, color, texture: arcade.texture, x, y):
         self.color = color
         self.value = value
         self.texture = texture
+        self.x = x
+        self.y = y
 
 
 class MyGame(arcade.Window):
@@ -45,11 +48,16 @@ class MyGame(arcade.Window):
         self.red_pieces = []
         self.blue_pieces = []
 
+        self.screen_width = width
+        self.screen_height = height
+
     def on_resize(self, width, height):
         """ This method is automatically called when the window is resized. """
 
         # Call the parent. Failing to do this will mess up the coordinates,
         # and default to 0,0 at the center and the edges being -1 to 1.
+        self.screen_width = width
+        self.screen_height = height
         super().on_resize(width, height)
 
     def setup(self):
@@ -64,25 +72,6 @@ class MyGame(arcade.Window):
                 texture_map_cords.append([PIECE_WIDTH * jj, PIECE_HEIGHT * ii, PIECE_WIDTH, PIECE_HEIGHT])
 
         # Create sprites and sprite lists here
-        self.blue_piece_textures = arcade.load_textures(
-            "img/blue_pieces.png",     # filename
-            texture_map_cords,
-            False,
-            False,
-            "Simple",
-            4.5
-        )
-
-        for ii in range (12):
-            if ii == 5:
-                self.red_pieces.append(Piece(-1, "red", self.red_piece_textures[ii]))
-            elif ii == 11:
-                self.red_pieces.append(Piece(ii, "red", self.red_piece_textures[ii]))
-            elif ii > 5:
-                self.red_pieces.append(Piece(10 - ii, "red", self.red_piece_textures[ii]))
-            else:
-                self.red_pieces.append(Piece(11 - ii, "red", self.red_piece_textures[ii]))
-
         self.red_piece_textures = arcade.load_textures(
             "img/red_pieces.png",
             texture_map_cords,
@@ -92,15 +81,121 @@ class MyGame(arcade.Window):
             4.5
         )
 
-        for ii in range (12):
-            if ii == 5:
-                self.blue_pieces.append(Piece(-1, "blue", self.blue_piece_textures[ii]))
-            elif ii == 11:
-                self.blue_pieces.append(Piece(ii, "red", self.blue_piece_textures[ii]))
-            elif ii > 5:
-                self.blue_pieces.append(Piece(10 - ii, "red", self.blue_piece_textures[ii]))
-            else:
-                self.blue_pieces.append(Piece(11 - ii, "red", self.blue_piece_textures[ii]))
+        # 40 total pieces for each player
+        # 1 Marshal
+        self.red_pieces.append(Piece(10, "red", self.red_piece_textures[0], self.width / 2, self.height / 2))
+        # 1 General
+        self.red_pieces.append(Piece(9, "red", self.red_piece_textures[1], self.width / 2, self.height / 2))
+        # 2 Colonels
+        self.red_pieces.append(Piece(8, "red", self.red_piece_textures[2], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(8, "red", self.red_piece_textures[2], self.width / 2, self.height / 2))
+        # 3 Majors
+        self.red_pieces.append(Piece(7, "red", self.red_piece_textures[3], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(7, "red", self.red_piece_textures[3], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(7, "red", self.red_piece_textures[3], self.width / 2, self.height / 2))
+        # 4 Captains
+        self.red_pieces.append(Piece(6, "red", self.red_piece_textures[4], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(6, "red", self.red_piece_textures[4], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(6, "red", self.red_piece_textures[4], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(6, "red", self.red_piece_textures[4], self.width / 2, self.height / 2))
+        # 4 Lieutenants
+        self.red_pieces.append(Piece(5, "red", self.red_piece_textures[6], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(5, "red", self.red_piece_textures[6], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(5, "red", self.red_piece_textures[6], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(5, "red", self.red_piece_textures[6], self.width / 2, self.height / 2))
+        # 4 Sergeants
+        self.red_pieces.append(Piece(4, "red", self.red_piece_textures[7], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(4, "red", self.red_piece_textures[7], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(4, "red", self.red_piece_textures[7], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(4, "red", self.red_piece_textures[7], self.width / 2, self.height / 2))
+        # 5 Miners
+        self.red_pieces.append(Piece(3, "red", self.red_piece_textures[8], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(3, "red", self.red_piece_textures[8], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(3, "red", self.red_piece_textures[8], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(3, "red", self.red_piece_textures[8], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(3, "red", self.red_piece_textures[8], self.width / 2, self.height / 2))
+        # 8 Scouts
+        self.red_pieces.append(Piece(2, "red", self.red_piece_textures[9], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(2, "red", self.red_piece_textures[9], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(2, "red", self.red_piece_textures[9], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(2, "red", self.red_piece_textures[9], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(2, "red", self.red_piece_textures[9], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(2, "red", self.red_piece_textures[9], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(2, "red", self.red_piece_textures[9], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(2, "red", self.red_piece_textures[9], self.width / 2, self.height / 2))
+        # 1 Spy
+        self.red_pieces.append(Piece(1, "red", self.red_piece_textures[10], self.width / 2, self.height / 2))
+        # 1 Flag
+        self.red_pieces.append(Piece(-1, "red", self.red_piece_textures[5], self.width / 2, self.height / 2))
+        # 6 Bombs
+        self.red_pieces.append(Piece(11, "red", self.red_piece_textures[11], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(11, "red", self.red_piece_textures[11], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(11, "red", self.red_piece_textures[11], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(11, "red", self.red_piece_textures[11], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(11, "red", self.red_piece_textures[11], self.width / 2, self.height / 2))
+        self.red_pieces.append(Piece(11, "red", self.red_piece_textures[11], self.width / 2, self.height / 2))
+
+        self.blue_piece_textures = arcade.load_textures(
+            "img/blue_pieces.png",  # filename
+            texture_map_cords,
+            False,
+            False,
+            "Simple",
+            4.5
+        )
+
+        # 1 Marshal
+        self.blue_pieces.append(Piece(10, "blue", self.blue_piece_textures[0], self.width / 2, self.height / 2))
+        # 1 General
+        self.blue_pieces.append(Piece(9, "blue", self.blue_piece_textures[1], self.width / 2, self.height / 2))
+        # 2 Colonels
+        self.blue_pieces.append(Piece(8, "blue", self.blue_piece_textures[2], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(8, "blue", self.blue_piece_textures[2], self.width / 2, self.height / 2))
+        # 3 Majors
+        self.blue_pieces.append(Piece(7, "blue", self.blue_piece_textures[3], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(7, "blue", self.blue_piece_textures[3], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(7, "blue", self.blue_piece_textures[3], self.width / 2, self.height / 2))
+        # 4 Captains
+        self.blue_pieces.append(Piece(6, "blue", self.blue_piece_textures[4], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(6, "blue", self.blue_piece_textures[4], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(6, "blue", self.blue_piece_textures[4], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(6, "blue", self.blue_piece_textures[4], self.width / 2, self.height / 2))
+        # 4 Lieutenants
+        self.blue_pieces.append(Piece(5, "blue", self.blue_piece_textures[6], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(5, "blue", self.blue_piece_textures[6], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(5, "blue", self.blue_piece_textures[6], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(5, "blue", self.blue_piece_textures[6], self.width / 2, self.height / 2))
+        # 4 Sergeants
+        self.blue_pieces.append(Piece(4, "blue", self.blue_piece_textures[7], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(4, "blue", self.blue_piece_textures[7], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(4, "blue", self.blue_piece_textures[7], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(4, "blue", self.blue_piece_textures[7], self.width / 2, self.height / 2))
+        # 5 Miners
+        self.blue_pieces.append(Piece(3, "blue", self.blue_piece_textures[8], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(3, "blue", self.blue_piece_textures[8], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(3, "blue", self.blue_piece_textures[8], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(3, "blue", self.blue_piece_textures[8], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(3, "blue", self.blue_piece_textures[8], self.width / 2, self.height / 2))
+        # 8 Scouts
+        self.blue_pieces.append(Piece(2, "blue", self.blue_piece_textures[9], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(2, "blue", self.blue_piece_textures[9], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(2, "blue", self.blue_piece_textures[9], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(2, "blue", self.blue_piece_textures[9], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(2, "blue", self.blue_piece_textures[9], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(2, "blue", self.blue_piece_textures[9], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(2, "blue", self.blue_piece_textures[9], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(2, "blue", self.blue_piece_textures[9], self.width / 2, self.height / 2))
+        # 1 Spy
+        self.blue_pieces.append(Piece(1, "blue", self.blue_piece_textures[10], self.width / 2, self.height / 2))
+        # 1 Flag
+        self.blue_pieces.append(Piece(-1, "blue", self.blue_piece_textures[5], self.width / 2, self.height / 2))
+        # 6 Bombs
+        self.blue_pieces.append(Piece(11, "blue", self.blue_piece_textures[11], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(11, "blue", self.blue_piece_textures[11], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(11, "blue", self.blue_piece_textures[11], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(11, "blue", self.blue_piece_textures[11], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(11, "blue", self.blue_piece_textures[11], self.width / 2, self.height / 2))
+        self.blue_pieces.append(Piece(11, "blue", self.blue_piece_textures[11], self.width / 2, self.height / 2))
 
     def on_draw(self):
         """
@@ -111,18 +206,35 @@ class MyGame(arcade.Window):
         # the screen to the background color, and erase what we drew last frame.
         self.clear()
 
-        arcade.draw_texture_rectangle(
-            # x, y, width, height, texture, angle, alpha
-            500,     # random temp value
-            500,     # random temp value
-            PIECE_WIDTH / 4,
-            PIECE_HEIGHT / 4,
-            self.blue_piece_textures[0],
-            0,
-            255
-        )
+        jj = 0
+        for ii in range(40):
+            arcade.draw_texture_rectangle(
+                # x, y, width, height, texture, angle, alpha
+                self.red_pieces[ii].x + (PIECE_WIDTH / 4) * (ii % 10),
+                self.red_pieces[ii].y + jj * (PIECE_HEIGHT / 4),
+                PIECE_WIDTH / 4,
+                PIECE_HEIGHT / 4,
+                self.red_pieces[ii].texture,
+                0,
+                255
+            )
+            if ii % 10 == 9:
+                jj += 1
 
-        # Call draw() on all the sprites here
+        jj = 0
+        for ii in range(40):
+            arcade.draw_texture_rectangle(
+                # x, y, width, height, texture, angle, alpha
+                self.blue_pieces[ii].x + PIECE_WIDTH / 4 * (ii % 10) - 600,
+                self.blue_pieces[ii].y + jj * PIECE_HEIGHT / 4,
+                PIECE_WIDTH / 4,
+                PIECE_HEIGHT / 4,
+                self.blue_pieces[ii].texture,
+                0,
+                255
+            )
+            if ii % 10 == 9:
+                jj += 1
 
     def on_update(self, delta_time):
         """
@@ -138,7 +250,10 @@ class MyGame(arcade.Window):
         For a full list of keys, see:
         https://api.arcade.academy/en/latest/arcade.key.html
         """
-        pass
+        if key == 65307:
+            arcade.close_window()
+        elif key == 114 and key_modifiers == 2:
+            self.setup()
 
     def on_key_release(self, key, key_modifiers):
         """
